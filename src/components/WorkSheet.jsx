2d5aef888e9133260a1de9d4a8bbeffd6717a4b7
Ahasan Ullah <ahasanullah451@gmail.com>
2025-01-19 02:57:11 +0600

    update method added and successfully fetched data

--- src/components/WorkSheet.jsx
+++ src/components/WorkSheet.jsx
@@ -11,7 +11,6 @@ import { MdDelete } from "react-icons/md";
 const WorkSheet = () => {
   const { user } = useContext(AuthContext);
   const [tasks, setTasks] = useState([]);
-  const [task,setTask]=useState([]);
   useEffect(() => {
     axios
       .get("http://localhost:5000/tasks", {
@@ -41,7 +40,6 @@ const WorkSheet = () => {
       date: data.date.toLocaleDateString("en-CA"),
       email: user.email,
     };
-    // adding user data to database
     axios
       .post("http://localhost:5000/tasks", taskData)
       .then((res) => {
@@ -56,15 +54,31 @@ const WorkSheet = () => {
       .catch((error) => {});
   };
 
-  const getTaskData=(id)=>{
-    axios.get('http://localhost:5000/tasks',{
-      params: {id: `${id}`}
-    }).then(res=>setTask(res.data));
-  }
-  
-  const handleUpdate=()=>{
+  const [newDate, setNewDate] = useState(null);
+  const [newTaskType, setNewTaskType] = useState(null);
+  const [newHourWorked, setNewHourWorked] = useState(null);
 
-  }
+  const getTaskData = (id) => {
+    axios
+      .get("http://localhost:5000/tasks", {
+        params: { id: `${id}` },
+      })
+      .then((res) => {
+        setNewDate(new Date(res.data[0].date));
+        setNewTaskType(res.data[0].taskType);
+        setNewHourWorked(res.data[0].hourWorked);
+      });
+  };
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    const updatedData={
+      taskType: newTaskType,
+      hourWorked: newHourWorked,
+      date: newDate.toLocaleDateString("en-CA")
+    }
+    console.log(updatedData);
+  };
 
   return (
     <div>
@@ -168,59 +182,67 @@ const WorkSheet = () => {
         </div>
       </div>
 
-
       {/* modal */}
       <dialog id="edit_modal" className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
-        <form
-          onSubmit={handleUpdate}
-          className="card-body md:col-span-4"
-        >
-          {/* task field */}
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Task Type</span>
-            </label>
-            <select
-              className="select select-bordered"
-              defaultValue={"Select Task"}
-            >
-              <option value="" disabled>
-                Select Task
-              </option>
-              <option value="Sales">Sales</option>
-              <option value="Support">Support</option>
-              <option value="Content">Conten</option>
-              <option value="Paper-Work">Paper-Work</option>
-            </select>
-          </div>
-          {/* work hour field */}
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Hour Wokred</span>
-            </label>
-            <input
-              type="number"
-              placeholder="Hour Worked"
-              className="input input-bordered"
-              required
-            />
-          </div>
-          {/* Date */}
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Date</span>
-            </label>
-            <DatePicker
-              className="input input-bordered w-full"
-              required
-            />
-          </div>
+          <form onSubmit={handleUpdate} className="card-body md:col-span-4">
+            {/* Task field */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Task Type</span>
+              </label>
+              <select
+                name="taskType"
+                className="select select-bordered"
+                defaultValue={newTaskType}
+                onChange={(e) => setNewTaskType(e.target.value)}
+              >
+                <option value="" disabled>
+                  Select Task
+                </option>
+                <option value="Sales">Sales</option>
+                <option value="Support">Support</option>
+                <option value="Content">Content</option>
+                <option value="Paper-Work">Paper-Work</option>
+              </select>
+            </div>
 
-          <div className="form-control mt-6">
-            <button className="btn btn-neutral">Add</button>
-          </div>
-        </form>
+            {/* Work hour field */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Hour Worked</span>
+              </label>
+              <input
+                type="number"
+                name="hourWorked"
+                placeholder="Hour Worked"
+                className="input input-bordered"
+                defaultValue={newHourWorked}
+                onChange={(e) => setNewHourWorked(e.target.value)}
+                required
+              />
+            </div>
+
+            {/* Date */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Date</span>
+              </label>
+              <DatePicker
+                selected={newDate}
+                onChange={(date) => setNewDate(date)}
+                className="input input-bordered w-full"
+                defaultValue={newDate}
+                required
+              />
+            </div>
+
+            <div className="form-control mt-6">
+              <button type="submit" className="btn btn-neutral">
+                Update
+              </button>
+            </div>
+          </form>
           <div className="modal-action">
             <form method="dialog">
               <button className="btn">Close</button>
