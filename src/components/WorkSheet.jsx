294553bf2e97bfab688c9d243f20af4d6d86ea87
Ahasan Ullah <ahasanullah451@gmail.com>
2025-01-20 09:46:52 +0600

    delete function modified

--- src/components/WorkSheet.jsx
+++ src/components/WorkSheet.jsx
@@ -106,7 +106,6 @@ const WorkSheet = () => {
   };
 
   const handleDelete=(id)=>{
-    console.log(id);
     Swal.fire({
       title: "Are you sure?",
       text: "You won't be able to revert this!",
