bd09c0408d1898d7e46eb3a88529da5652d39f4e
Ahasan Ullah <ahasanullah451@gmail.com>
2025-01-23 01:09:45 +0600

    feedback table added

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,7 +1,30 @@
+import axios from "axios";
+import { useState } from "react";
+
 const Feedback = () => {
+  const [feedbacks,setFeedbacks]=useState([]);
+  axios.get('http://localhost:5000/feedback').then(res=>{setFeedbacks(res.data)})
   return (
-    <div>
-      
+    <div className="p-6">
+      <h2 className="text-3xl font-bold text-center mb-4">User's Feedback</h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full border">
+          <thead>
+            <tr>
+              <th className="border">Email</th>
+              <th className="border">Message</th>
+            </tr>
+          </thead>
+          <tbody>
+            {feedbacks.map((feedback) => (
+              <tr key={feedback._id}>
+                <td className="border">{feedback.email}</td>
+                <td className="border">{feedback.message}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
